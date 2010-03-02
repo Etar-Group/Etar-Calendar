b1d8cfcf0834a1c92d138babff11e2c9bbf1dfe4
Ken Shirriff <kens@google.com>
2010-03-02 13:15:27 -0800

    Don't unbundle calendar app.
    The calendar.jar didn't work for unbunding.
    bug 2468654

--- Android.mk
+++ Android.mk
@@ -5,14 +5,11 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under,src)
 
-LOCAL_SDK_VERSION := current
-
 # TODO: Remove dependency of application on the test runner (android.test.runner)
 # library.
 LOCAL_JAVA_LIBRARIES := android.test.runner
 
 LOCAL_STATIC_JAVA_LIBRARIES += android-common
-LOCAL_STATIC_JAVA_LIBRARIES += calendar # Shared calendar code
 
 LOCAL_PACKAGE_NAME := Calendar
 
