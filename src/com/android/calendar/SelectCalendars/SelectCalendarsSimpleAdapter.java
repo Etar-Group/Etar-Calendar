975940b44da39c68805df6d08012374f3fa35969
Erik <roboerik@android.com>
2010-07-16 13:23:59 -0700

    Adds functionality back into the mini SelectCalendars view
    This allows calendar visibility to be toggled by the mini view
    and draws a filled or stroked box to represent the status of each
    calendar.
    
    Change-Id: Ie1280872b2e10e7dd52434cded9b2494003f8a58

--- src/com/android/calendar/SelectCalendars/SelectCalendarsSimpleAdapter.java
+++ src/com/android/calendar/SelectCalendars/SelectCalendarsSimpleAdapter.java
@@ -17,44 +17,117 @@
 package com.android.calendar.SelectCalendars;
 
 import com.android.calendar.R;
-import com.android.calendar.R.id;
-import com.android.calendar.Utils;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.Paint;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RectShape;
 import android.provider.Calendar.Calendars;
 import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ResourceCursorAdapter;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
 import android.widget.TextView;
 
-public class SelectCalendarsSimpleAdapter extends ResourceCursorAdapter {
+public class SelectCalendarsSimpleAdapter extends BaseAdapter implements ListAdapter {
+    private static final int SELECTED_BOX_BORDER = 4;
+    private static int COLOR_CHIP_SIZE = 30;
+    private RectShape r = new RectShape();
 
-    public SelectCalendarsSimpleAdapter(Context context, int layout, Cursor c) {
-        super(context, layout, c);
+    private LayoutInflater mInflater;
+    private int mLayout;
+    private CalendarRow[] mData;
+    private Cursor mCursor;
+    private int mRowCount = 0;
+
+    private int mIdColumn;
+    private int mNameColumn;
+    private int mColorColumn;
+    private int mSelectedColumn;
+
+    private class CalendarRow {
+        long id;
+        String displayName;
+        int color;
+        boolean selected;
     }
 
+    public SelectCalendarsSimpleAdapter(Context context, int layout, Cursor c) {
+        super();
+        mLayout = layout;
+        initData(c);
+        mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        COLOR_CHIP_SIZE *= context.getResources().getDisplayMetrics().density;
+        r.resize(COLOR_CHIP_SIZE, COLOR_CHIP_SIZE);
+    }
 
-    @Override
-    public void bindView(View view, Context context, Cursor cursor) {
-//        int ownerColumn = cursor.getColumnIndexOrThrow(Calendars.OWNER_ACCOUNT);
-        int nameColumn = cursor.getColumnIndexOrThrow(Calendars.DISPLAY_NAME);
-        int colorColumn = cursor.getColumnIndexOrThrow(Calendars.COLOR);
-        String name = cursor.getString(nameColumn);
-        view.findViewById(R.id.color)
-            .setBackgroundDrawable(Utils.getColorChip(cursor.getInt(colorColumn)));
-        // TODO notify user in status if the account isn't auto-syncing
-//        String owner = cursor.getString(ownerColumn);
-//        if (mIsDuplicateName.containsKey(name) && mIsDuplicateName.get(name) &&
-//                !name.equalsIgnoreCase(owner)) {
-//            name = new StringBuilder(name)
-//                    .append(Utils.OPEN_EMAIL_MARKER)
-//                    .append(owner)
-//                    .append(Utils.CLOSE_EMAIL_MARKER)
-//                    .toString();
-//        }
+    private void initData(Cursor c) {
+        if (mCursor != null && c != mCursor) {
+            mCursor.close();
+        }
+        if (c == null) {
+            mCursor = c;
+            mRowCount = 0;
+            mData = null;
+            return;
+        }
+        // TODO create a broadcast listener for ACTION_PROVIDER_CHANGED to update the cursor
+        mCursor = c;
+        mIdColumn = c.getColumnIndexOrThrow(Calendars._ID);
+        mNameColumn = c.getColumnIndexOrThrow(Calendars.DISPLAY_NAME);
+        mColorColumn = c.getColumnIndexOrThrow(Calendars.COLOR);
+        mSelectedColumn = c.getColumnIndexOrThrow(Calendars.SELECTED);
+
+        mRowCount = c.getCount();
+        mData = new CalendarRow[(c.getCount() + 2)];
+        c.moveToPosition(-1);
+        int p = 0;
+        while (c.moveToNext()) {
+            mData[p] = new CalendarRow();
+            mData[p].id = c.getLong(mIdColumn);
+            mData[p].displayName = c.getString(mNameColumn);
+            mData[p].color = c.getInt(mColorColumn);
+            mData[p].selected = c.getInt(mSelectedColumn) != 0;
+            p++;
+        }
+    }
+
+    public void changeCursor(Cursor c) {
+        initData(c);
+        notifyDataSetChanged();
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (position >= mRowCount) {
+            return null;
+        }
+        String name = mData[position].displayName;
+        boolean selected = mData[position].selected;
+        int color = mData[position].color;
+        View view;
+        if (convertView == null) {
+            view = mInflater.inflate(mLayout, parent, false);
+        } else {
+            view = convertView;
+        }
+
+        View colorView = view.findViewById(R.id.color);
+        ShapeDrawable box = new ShapeDrawable(r);
+        Paint p = box.getPaint();
+        p.setColor(color);
+        if (selected) {
+            p.setStyle(Paint.Style.FILL_AND_STROKE);
+        } else {
+            p.setStyle(Paint.Style.STROKE);
+            p.setStrokeWidth(SELECTED_BOX_BORDER);
+        }
+
+        colorView.setBackgroundDrawable(box);
         setText(view, R.id.calendar, name);
-//        setText(view, R.id.status, status);
+        return view;
     }
 
     private static void setText(View view, int id, String text) {
@@ -63,6 +136,46 @@ public class SelectCalendarsSimpleAdapter extends ResourceCursorAdapter {
         }
         TextView textView = (TextView) view.findViewById(id);
         textView.setText(text);
+    }
+
+
+    /* (non-Javadoc)
+     * @see android.widget.Adapter#getCount()
+     */
+    public int getCount() {
+        return mRowCount;
+    }
+
+
+    /* (non-Javadoc)
+     * @see android.widget.Adapter#getItem(int)
+     */
+    public Object getItem(int position) {
+        if (position >= mRowCount) {
+            return null;
+        }
+        CalendarRow item = mData[position];
+        return item;
+    }
+
+
+    /* (non-Javadoc)
+     * @see android.widget.Adapter#getItemId(int)
+     */
+    public long getItemId(int position) {
+        if (position >= mRowCount) {
+            return 0;
+        }
+        return mData[position].id;
+    }
+
+    public void setVisible(int position, int visible) {
+        mData[position].selected = visible != 0;
+        notifyDataSetChanged();
+    }
+
+    public int getVisible(int position) {
+        return mData[position].selected ? 1 : 0;
     }
 
 }
