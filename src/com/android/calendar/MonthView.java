6ab336ae860b4b712720eb440cb5d9aadd1c3e74
James Peterson <james.d.peterson@gmail.com>
2011-01-19 12:13:19 -0600

    Updated MonthView   Added Color highlighting to the dates instead of a black bold
    Change-Id: I5f29c0008294e9fbe12f0ecfbabca511ec4c4aed

--- src/com/android/calendar/MonthView.java
+++ src/com/android/calendar/MonthView.java
@@ -878,7 +878,17 @@ public class MonthView extends View implements View.OnCreateContextMenuListener 
                 p.setColor(mMonthDayNumberColor);
             }
             //bolds the day if there's an event that day
-            p.setFakeBoldText(eventDay[day-mFirstJulianDay]);
+			p.setFakeBoldText(false);
+            ArrayList<Event> events = mEvents;
+            if (eventDay[day-mFirstJulianDay]) {
+                for(int e = 0;e < events.size();e++) {
+                    Event event = events.get(e);
+                    if (event.startDay <= day && event.endDay >= day && event.allDay) {
+                        p.setFakeBoldText(true);
+                        p.setColor(event.color);
+                    }
+                }
+            }
         }
         /*Drawing of day number is done here
          *easy to find tags draw number draw day*/
