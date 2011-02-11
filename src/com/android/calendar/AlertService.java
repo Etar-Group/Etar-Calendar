ed7fa72c1636396a99600d1436ff8a0b08a2cf99
Michael Chan <mchan@android.com>
2011-02-10 18:20:20 -0800

    b/3431141 Fixed the case where multiple alerts wont sound. DO NOT MERGE
    This affects all events with multiple alerts.
    Only the first one makes a sound.
    The ones after that fire silently without vibration.
    
    Change-Id: Icbe30b9c92675e94bd5dd1d0456fe510238ce708

--- src/com/android/calendar/AlertService.java
+++ src/com/android/calendar/AlertService.java
@@ -180,9 +180,13 @@ public class AlertService extends Service {
                 //     newState = CalendarAlerts.DISMISSED;
                 // } else
 
-                // Remove declined events and duplicate alerts for the same event
-                if (!declined && eventIds.put(eventId, beginTime) == null) {
-                    numReminders++;
+                // Remove declined events
+                if (!declined) {
+                    // Don't count duplicate alerts for the same event
+                    if (eventIds.put(eventId, beginTime) == null) {
+                        numReminders++;
+                    }
+
                     if (state == CalendarAlerts.SCHEDULED) {
                         newState = CalendarAlerts.FIRED;
                         numFired++;
@@ -194,9 +198,6 @@ public class AlertService extends Service {
                     }
                 } else {
                     newState = CalendarAlerts.DISMISSED;
-                    if (DEBUG) {
-                        if (!declined) Log.d(TAG, "dropping dup alert for event " + eventId);
-                    }
                 }
 
                 // Update row if state changed
