99e777af3cb9b4556f86477b7b8402d87f723f54
RoboErik <epastern@google.com>
2011-07-29 11:00:53 -0700

    b/5092650 Put app up button in phone settings
    Change-Id: I29d15ff75a17837eee3eebef15f46f253fe64247

--- src/com/android/calendar/CalendarSettingsActivity.java
+++ src/com/android/calendar/CalendarSettingsActivity.java
@@ -39,21 +39,13 @@ public class CalendarSettingsActivity extends PreferenceActivity {
     private static final int CHECK_ACCOUNTS_DELAY = 3000;
     private Account[] mAccounts;
     private Handler mHandler = new Handler();
-    private static boolean mIsMultipane;
 
     @Override
     public void onBuildHeaders(List<Header> target) {
         loadHeadersFromResource(R.xml.calendar_settings_headers, target);
 
-        mIsMultipane = Utils.isMultiPaneConfiguration (this);
-        if (mIsMultipane) {
-            getActionBar().setDisplayOptions(
-                    ActionBar.DISPLAY_HOME_AS_UP, ActionBar.DISPLAY_HOME_AS_UP);
-        }
-        else {
-            getActionBar().setDisplayOptions(0,
-                    ActionBar.DISPLAY_HOME_AS_UP | ActionBar.DISPLAY_SHOW_HOME);
-        }
+        getActionBar()
+                .setDisplayOptions(ActionBar.DISPLAY_HOME_AS_UP, ActionBar.DISPLAY_HOME_AS_UP);
 
         Account[] accounts = AccountManager.get(this).getAccounts();
         if (accounts != null) {
