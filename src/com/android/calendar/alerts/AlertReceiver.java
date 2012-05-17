aacf42df2c9fb84203a519ec73c2a52987ee2195
Sara Ting <sarating@google.com>
2012-05-16 17:57:02 -0700

    Icons for notifications and email attendees button in event info.
    Change-Id: If3c0190aceae92cf9efe95afe96d2e0a2e642aaa

--- src/com/android/calendar/alerts/AlertReceiver.java
+++ src/com/android/calendar/alerts/AlertReceiver.java
@@ -44,7 +44,6 @@ import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -251,8 +250,8 @@ public class AlertReceiver extends BroadcastReceiver {
             // Create a snooze button.  TODO: change snooze to 10 minutes.
             PendingIntent snoozeIntent = createSnoozeIntent(context, eventId, startMillis,
                     endMillis, notificationId);
-            notificationBuilder.addAction(R.drawable.snooze,
-                    resources.getString(R.string.snooze_5min_label), snoozeIntent);
+            notificationBuilder.addAction(R.drawable.ic_alarm_holo_dark,
+                    resources.getString(R.string.snooze_label), snoozeIntent);
 
             // Create an email button.
             PendingIntent emailIntent = createBroadcastMailIntent(context, eventId, title);
