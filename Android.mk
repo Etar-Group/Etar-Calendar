bb768dfe3a03187401b9476cacbdcc5cbcfa9bab
Isaac Katzenelson <isaack@android.com>
2012-06-14 17:32:40 -0700

    Fix task stack building on ICS
    Change-Id: I2f4ed27c49d339ceb4424cfe7457e645c9cc2ad6

--- Android.mk
+++ Android.mk
@@ -21,6 +21,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under,src)
 LOCAL_STATIC_JAVA_LIBRARIES := \
 		android-common \
 		android-common-chips \
+		android-support-v4 \
 		calendar-common
 LOCAL_SDK_VERSION := current
 
