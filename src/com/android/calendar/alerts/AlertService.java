3ca997a6ebd5fc29943feea73713fbd48bbf1429
DvTonder <david.vantonder@gmail.com>
2012-10-06 18:36:03 -0400

    Calendar: Enable the LED for calendar alerts
    This will trigger the LED on calendar alerts and allow the user to
    override the color etc in Notification light settings when picking the
    Calendar app.
    
    Change-Id: I06d7ecb533e163b6ef5f959b342c56dedb923988

--- src/com/android/calendar/alerts/AlertService.java
+++ src/com/android/calendar/alerts/AlertService.java
@@ -742,7 +742,10 @@ public class AlertService extends Service {
     private static void addNotificationOptions(NotificationWrapper nw, boolean quietUpdate,
             String tickerText, boolean defaultVibrate, String reminderRingtone) {
         Notification notification = nw.mNotification;
+
+        // Set the lights defaults
         notification.defaults |= Notification.DEFAULT_LIGHTS;
+        notification.flags |= Notification.FLAG_SHOW_LIGHTS;
 
         // Quietly update notification bar. Nothing new. Maybe something just got deleted.
         if (!quietUpdate) {
