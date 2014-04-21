e1f2fccfd9ff54966e0979e17bdfbf06daa273dd
kaiyiz <kaiyiz@codeaurora.org>
2014-04-21 18:15:33 +0800

    Calendar: The delete events option status changed after rotate screen
    Calendar through LoadManager to query the total number of events, and
    update the delete events option status in onLoadFinished method. But
    this method will be called at the first time load data or data have
    changed, so can't update option status when rotating screen.
    
    Calls the initLoader function in onCreate and onResume ensure that
    the onLoadFinished can be callback.
    
    CRs-Fixed: 650667
    
    Change-Id: Id96f28bf648de374fb5d2cb7e0827e072f5342bd

--- src/com/android/calendar/AllInOneActivity.java
+++ src/com/android/calendar/AllInOneActivity.java
@@ -471,6 +471,9 @@ public class AllInOneActivity extends AbstractCalendarActivity implements EventH
         prefs.registerOnSharedPreferenceChangeListener(this);
 
         mContentResolver = getContentResolver();
+        if (getResources().getBoolean(R.bool.show_delete_events_menu)) {
+            getLoaderManager().initLoader(0, null, this);
+        }
     }
 
     private long parseViewAction(final Intent intent) {
@@ -595,6 +598,9 @@ public class AllInOneActivity extends AbstractCalendarActivity implements EventH
         invalidateOptionsMenu();
 
         mCalIntentReceiver = Utils.setTimeChangesReceiver(this, mTimeChangesUpdater);
+        if (getResources().getBoolean(R.bool.show_delete_events_menu)) {
+            getLoaderManager().initLoader(0, null, this);
+        }
     }
 
     @Override
