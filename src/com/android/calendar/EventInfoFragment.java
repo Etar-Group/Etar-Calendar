cb629af221539821b761717c306c3b8e25a37662
Matt Garnes <matt@cyngn.com>
2014-12-09 02:47:07 +0100

    Support sharing calendar events as .vcs files to MMS
    - Export calendar events to a temporary .vcs file. Currently, the actual
      file format is still ical, this may need to be reformatted to actually
      contain only vCalendar content.
    - Share this specifically to the com.android.mms package when the
      share button is clicked.
    - Share the event as ical .ics to all other applications.
    
    Change-Id: If337360b17d4071d580087ecc97a2397d0563ff6
    (cherry picked from commit a91c50a)

--- src/com/android/calendar/EventInfoFragment.java
+++ src/com/android/calendar/EventInfoFragment.java
@@ -1167,7 +1167,27 @@ public class EventInfoFragment extends DialogFragment implements OnCheckedChange
                 // The ics file is sent as an extra, the receiving application decides whether to
                 // parse the file to extract calendar events or treat it as a regular file
                 shareIntent.setType("application/octet-stream");
-                startActivity(shareIntent);
+
+                Intent chooserIntent = Intent.createChooser(shareIntent,
+                                        getResources().getString(R.string.cal_share_intent_title));
+
+                // The MMS app only responds to "text/x-vcalendar" so we create a chooser intent
+                // that includes the targeted mms intent + any that respond to the above general
+                // purpose "application/octet-stream" intent.
+                File vcsInviteFile = File.createTempFile(filePrefix, ".vcs",
+                    mActivity.getExternalCacheDir());
+                // For now, we are duplicating ics file and using that as the vcs file
+                // TODO: revisit above
+                if (IcalendarUtils.copyFile(inviteFile, vcsInviteFile)) {
+                    Intent mmsShareIntent = new Intent();
+                    mmsShareIntent.setAction(Intent.ACTION_SEND);
+                    mmsShareIntent.setPackage("com.android.mms");
+                    mmsShareIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(vcsInviteFile));
+                    mmsShareIntent.setType("text/x-vcalendar");
+                    chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS,
+                                           new Intent[]{mmsShareIntent});
+                }
+                startActivity(chooserIntent);
                 isShareSuccessful = true;
 
             } else {
