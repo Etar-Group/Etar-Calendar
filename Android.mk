9d22dd9ae64221acc1038aae07d2e3972b8f884b
daktak <daktak@domo.ddns.net>
2016-11-04 10:44:25 +1100

    Update Android.mk
    Use gradle to build

--- Android.mk
+++ Android.mk
@@ -1,50 +1,27 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-# Include res dir from chips
-chips_dir := ../../../frameworks/ex/chips/res
-color_picker_dir := ../../../frameworks/opt/colorpicker/res
-datetimepicker_dir := ../../../frameworks/opt/datetimepicker/res
-timezonepicker_dir := ../../../frameworks/opt/timezonepicker/res
-res_dirs := $(chips_dir) $(color_picker_dir) $(datetimepicker_dir) $(timezonepicker_dir) res
-src_dirs := src
-
-LOCAL_EMMA_COVERAGE_FILTER := +com.android.calendar.*
-
+LOCAL_MODULE := Calendar
 LOCAL_MODULE_TAGS := optional
-
-LOCAL_SRC_FILES := $(call all-java-files-under,$(src_dirs))
-
-# bundled
-#LOCAL_STATIC_JAVA_LIBRARIES += \
-#        android-common \
-#        android-common-chips \
-#        calendar-common
-
-# unbundled
-LOCAL_STATIC_JAVA_LIBRARIES := \
-        android-common \
-        android-common-chips \
-        colorpicker \
-        android-opt-datetimepicker \
-        android-opt-timezonepicker \
-        android-support-v4 \
-        calendar-common
-
-LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, $(res_dirs))
-
 LOCAL_PACKAGE_NAME := Calendar
 
-LOCAL_PROGUARD_FLAG_FILES := proguard.flags \
-                             ../../../frameworks/opt/datetimepicker/proguard.flags
+canedar_root  := $(LOCAL_PATH)
+canedar_out   := $(OUT_DIR)/target/common/obj/APPS/$(LOCAL_MODULE)_intermediates
+canedar_build := $(canedar_root)/build
+canedar_apk   := build/outputs/apk/Calendar-release-unsigned.apk
 
-LOCAL_AAPT_FLAGS := --auto-add-overlay
-LOCAL_AAPT_FLAGS += --extra-packages com.android.ex.chips
-LOCAL_AAPT_FLAGS += --extra-packages com.android.colorpicker
-LOCAL_AAPT_FLAGS += --extra-packages com.android.datetimepicker
-LOCAL_AAPT_FLAGS += --extra-packages com.android.timezonepicker
+$(canedar_root)/$(canedar_apk):
+	rm -Rf $(canedar_build)
+	mkdir -p $(canedar_out)
+	ln -s $(canedar_out) $(canedar_build)
+	echo "sdk.dir=$(ANDROID_HOME)" > $(canedar_root)/local.properties
+	cd $(canedar_root) && git submodule update --recursive --init
+	cd $(canedar_root) && gradle dependencies && JAVA_TOOL_OPTIONS="$(JAVA_TOOL_OPTIONS) -Dfile.encoding=UTF8" gradle assembleRelease
 
-include $(BUILD_PACKAGE)
+LOCAL_CERTIFICATE := platform
+LOCAL_SRC_FILES := $(canedar_apk)
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_PATH := $(TARGET_OUT_DATA)/app
 
-# Use the following include to make our test apk.
-include $(call all-makefiles-under,$(LOCAL_PATH))
+include $(BUILD_PREBUILT)
