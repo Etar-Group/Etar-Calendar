d16d79e020d9063f18ab7bd9795f8e86c3dedb6f
Gitsaibot <tr.man@gmx.de>
2016-12-18 01:11:50 +0100

    Change unique address and add translations

--- src/com/android/calendar/icalendar/VEvent.java
+++ src/com/android/calendar/icalendar/VEvent.java
@@ -85,7 +85,7 @@ public class VEvent {
         mAttendees = new LinkedList<Attendee>();
 
         // Generate and add a unique identifier to this event - iCal requisite
-        addProperty(UID , UUID.randomUUID().toString() + "@cyanogenmod.com");
+        addProperty(UID , UUID.randomUUID().toString() + "@ws.xsoh.etar");
         addTimeStamp();
     }
 
