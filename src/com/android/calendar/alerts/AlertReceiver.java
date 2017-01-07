b56f29665430779be894eae73a55564dc0c4eba7
Gitsaibot <tr.man@gmx.de>
2017-01-07 11:37:50 +0100

    Calendar: Do not show "Map" action if app cannot be resolved
    Sometimes the phone didn't contains the map application, and there isn't
    any application to resolve the view map action.
    
    In this case, the Calendar needn't to show the "Map" action on the statusbar.
    
    CRs-fixed: 619204
    
    Change-Id: I475a3285b23cfbac3a078fb6e89ca6c46575e9bf
    (cherry picked from commit 94223ed)

--- src/com/android/calendar/alerts/AlertReceiver.java
+++ src/com/android/calendar/alerts/AlertReceiver.java
@@ -24,6 +24,8 @@ import android.content.BroadcastReceiver;
 import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.content.res.Resources;
 import android.database.Cursor;
 import android.net.Uri;
@@ -299,6 +301,13 @@ public class AlertReceiver extends BroadcastReceiver {
                 context, title, summaryText, startMillis, endMillis, eventId, notificationId,
                 doPopup, priority, false);
         return new NotificationWrapper(n, notificationId, eventId, startMillis, endMillis, doPopup);
+    }
+
+    public static boolean isResolveIntent(Context context, Intent intent) {
+        final PackageManager packageManager = context.getPackageManager();
+        List<ResolveInfo> resolveInfo = packageManager.queryIntentActivities(intent,
+                PackageManager.MATCH_DEFAULT_ONLY);
+        return (resolveInfo.size() > 0);
     }
 
     private static Notification buildBasicNotification(Notification.Builder notificationBuilder,
@@ -787,7 +796,7 @@ public class AlertReceiver extends BroadcastReceiver {
     /**
      * Create a pending intent to send ourself a broadcast to start maps, using the first map
      * link available.
-     * If no links are found, return null.
+     * If no links or resolved applications are found, return null.
      */
     private static PendingIntent createMapBroadcastIntent(Context context, URLSpan[] urlSpans,
             long eventId) {
@@ -795,12 +804,17 @@ public class AlertReceiver extends BroadcastReceiver {
             URLSpan urlSpan = urlSpans[span_i];
             String urlString = urlSpan.getURL();
             if (urlString.startsWith(GEO_PREFIX)) {
-                Intent broadcastIntent = new Intent(MAP_ACTION);
-                broadcastIntent.setClass(context, AlertReceiver.class);
-                broadcastIntent.putExtra(EXTRA_EVENT_ID, eventId);
-                return PendingIntent.getBroadcast(context,
-                        Long.valueOf(eventId).hashCode(), broadcastIntent,
-                        PendingIntent.FLAG_CANCEL_CURRENT);
+                Intent geoIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(urlString));
+                geoIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                // If this intent cannot be handled, do not create the map action
+                if (isResolveIntent(context, geoIntent)) {
+                    Intent broadcastIntent = new Intent(MAP_ACTION);
+                    broadcastIntent.setClass(context, AlertReceiver.class);
+                    broadcastIntent.putExtra(EXTRA_EVENT_ID, eventId);
+                    return PendingIntent.getBroadcast(context,
+                            Long.valueOf(eventId).hashCode(), broadcastIntent,
+                            PendingIntent.FLAG_CANCEL_CURRENT);
+                }
             }
         }
 
