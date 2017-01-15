287af8b258660aa6dfef59c2ff90e3a39bb8bea9
Gitsaibot <tr.man@gmx.de>
2017-01-15 01:00:39 +0100

    Only import used java.io classes. Delete unused cm_strings

--- src/com/android/calendar/icalendar/IcalendarUtils.java
+++ src/com/android/calendar/icalendar/IcalendarUtils.java
@@ -24,7 +24,15 @@ import android.system.ErrnoException;
 import android.system.OsConstants;
 import com.android.calendar.CalendarEventModel;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
