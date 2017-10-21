aa12aa4d14cd10f84772a97b84655956684607bf
Gitsaibot <Gitsaibot@users.noreply.github.com>
2017-10-21 19:34:26 +0200

    Calendar: Support display lunar/festival when Language is Chinese (#263)
    If the current language is set as Chinese,Calendar could display
    the lunar and festival for month,week,and day view.
    
    Change-Id: Ie6baff4257939e2a99fc173d57e18651618044b5
    
    (cherry picked from commit 29b4d09)

--- src/com/android/calendar/DayView.java
+++ src/com/android/calendar/DayView.java
@@ -652,6 +652,10 @@ public class DayView extends View implements View.OnCreateContextMenuListener,
         }
         HOURS_MARGIN = HOURS_LEFT_MARGIN + HOURS_RIGHT_MARGIN;
         DAY_HEADER_HEIGHT = mNumDays == 1 ? ONE_DAY_HEADER_HEIGHT : MULTI_DAY_HEADER_HEIGHT;
+        if (LunarUtils.showLunar(mContext) && mNumDays != 1) {
+            DAY_HEADER_HEIGHT = (int) (DAY_HEADER_HEIGHT + DAY_HEADER_FONT_SIZE + 2);
+        }
+
         mCurrentTimeLine = mResources.getDrawable(R.drawable.timeline_indicator_holo_light);
         mCurrentTimeAnimateLine = mResources
                 .getDrawable(R.drawable.timeline_indicator_activated_holo_light);
@@ -2503,8 +2507,12 @@ public class DayView extends View implements View.OnCreateContextMenuListener,
         // Draw day of the month
         String dateNumStr = String.valueOf(dateNum);
         if (mNumDays > 1) {
-            float y = DAY_HEADER_HEIGHT - DAY_HEADER_BOTTOM_MARGIN;
-
+            float y = -1;
+            if (LunarUtils.showLunar(mContext)) {
+                y = DAY_HEADER_HEIGHT - DAY_HEADER_BOTTOM_MARGIN - DATE_HEADER_FONT_SIZE - 2;
+            } else {
+                y = DAY_HEADER_HEIGHT - DAY_HEADER_BOTTOM_MARGIN;
+            }
             // Draw day of the month
             x = computeDayLeftPosition(day) + DAY_HEADER_RIGHT_MARGIN;
             p.setTextAlign(Align.LEFT);
@@ -2519,6 +2527,27 @@ public class DayView extends View implements View.OnCreateContextMenuListener,
             p.setTextSize(DAY_HEADER_FONT_SIZE);
             p.setTypeface(Typeface.DEFAULT);
             canvas.drawText(dayStr, x, y, p);
+
+            // To show the lunar info.
+            if (LunarUtils.showLunar(mContext)) {
+                // adjust the year and month
+                int month = mBaseDate.month;
+                int year = mBaseDate.year;
+                if (dateNum > mMonthLength || dateNum < mFirstVisibleDate) {
+                    month = month + 1;
+                    if (month > 11) {
+                        month = 0;
+                        year = year + 1;
+                    }
+                }
+
+                String lunarInfo = LunarUtils.get(mContext, year, month, dateNum,
+                        LunarUtils.FORMAT_LUNAR_SHORT | LunarUtils.FORMAT_ONE_FESTIVAL,
+                        false, null);
+                if (!TextUtils.isEmpty(lunarInfo)) {
+                    canvas.drawText(lunarInfo, x, y + DAY_HEADER_FONT_SIZE + 2, p);
+                }
+            }
         } else {
             float y = ONE_DAY_HEADER_HEIGHT - DAY_HEADER_ONE_DAY_BOTTOM_MARGIN;
             p.setTextAlign(Align.LEFT);
