b897058f1826441b1701ae143e8932076950cb24
Timo Wilken <timo.21.wilken@gmail.com>
2018-05-13 16:26:23 +0100

    Use dynamic lookup for colors.

--- src/com/android/calendar/DynamicTheme.java
+++ src/com/android/calendar/DynamicTheme.java
@@ -1,7 +1,11 @@
 package com.android.calendar;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Color;
+
 import ws.xsoh.etar.R;
 
 /**
@@ -37,6 +41,17 @@ public class DynamicTheme {
             return R.style.CalendarAppThemeBlack;
 
         return R.style.CalendarAppTheme;
+    }
+
+    public int getColor(Context context, String id) {
+        String theme = Utils.getTheme(context), suffix = "";
+        if (theme.equals(DARK)) {
+            suffix = "_dark";
+        } else if (theme.equals(BLACK)) {
+            suffix = "_black";
+        }
+        Resources res = context.getResources();
+        return res.getColor(res.getIdentifier(id + suffix, "color", context.getPackageName()));
     }
 
     private static final class OverridePendingTransition {
