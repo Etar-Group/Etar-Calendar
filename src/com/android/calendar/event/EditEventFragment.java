Etar-Group/Etar-Calendar

commit ea6576c6aea9b407aa1b4fbc8adaba34deeaa031

Close the EditEvent view upon pause (#367)
When switching apps, the onPause() function saves the current event. But
if the user switches back, the view is still open and if the save button
is pressed, two events are created. This closes the view, so the user
would have to open it again, to edit it.

An alternative solution would be to create the event only temporary and
not save it in onPause(), as discussed in #361.

src/com/android/calendar/event/EditEventFragment.java
@@ -574,6 +574,7 @@ public class EditEventFragment extends Fragment implements EventHandler, OnColor
             mOnDone.setDoneCode(Utils.DONE_SAVE);
             mOnDone.run();
         }
+        act.finish();
         super.onPause();
     }
 

commit 92e853a1e7d4c3dbec71546154714b35860e1641

Allow edit events without contact permission (Closes: #422)
Don't close the edit view when asking for contact permission. This broke
with #367.

src/com/android/calendar/event/EditEventFragment.java
@@ -575,7 +575,10 @@ public class EditEventFragment extends Fragment implements EventHandler, OnColor
             mOnDone.setDoneCode(Utils.DONE_SAVE);
             mOnDone.run();
         }
-        act.finish();
+        if (act !=null && (Build.VERSION.SDK_INT < 23 ||
+                    ContextCompat.checkSelfPermission(EditEventFragment.this.getActivity(),
+                        Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED))
+            act.finish();
         super.onPause();
     }
 