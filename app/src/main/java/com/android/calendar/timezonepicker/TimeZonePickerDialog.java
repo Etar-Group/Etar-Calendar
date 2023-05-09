c4ed15a16016d4ef0bb7a0b456ec391e5acf8b77
Aayush Gupta <aayushgupta219@gmail.com>
2023-05-09 22:02:00 +0530

    Migrate to androidx fragment library
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- app/src/main/java/com/android/calendar/timezonepicker/TimeZonePickerDialog.java
+++ app/src/main/java/com/android/calendar/timezonepicker/TimeZonePickerDialog.java
@@ -17,13 +17,14 @@
 package com.android.calendar.timezonepicker;
 
 import android.app.Dialog;
-import android.app.DialogFragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+
+import androidx.fragment.app.DialogFragment;
 
 public class TimeZonePickerDialog extends DialogFragment implements
         TimeZonePickerView.OnTimeZoneSetListener {
