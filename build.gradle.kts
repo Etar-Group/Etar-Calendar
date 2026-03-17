45ffa63a19009c74ab1b0f7090f57107d06331b8
Gitsaibot <Gitsaibot@exmaple.com>
2026-03-17 14:31:08 +0100

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.ec4j.editorconfig) apply false
     alias(libs.plugins.jetbrains.idea.ext) apply true
 }
